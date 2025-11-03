a9576e2e057302d04c613e7a5353d5bf65b82f1a
Apurva Satish Ratnakar <apurvasatishratnakar1409@gmail.com>
2025-11-04 01:21:13 +0530

    program3.c
    This program prints numbers from 5 down to 1 using a while loop.
    
    Inside the Display() function, a variable icnt is set to 5.
    
    The while loop runs as long as icnt is greater than or equal to 1.
    
    In every iteration, the current value of icnt is printed.
    
    After printing, icnt is decreased (icnt--), so the loop counts backward.
    
    The main() function calls Display() to execute the loop.

---
+++ Assignments/Assignment_1/program3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int Display()
+{
+    int icnt=0;
+ icnt=5;
+
+    while(icnt>=1)
+    {
+        printf("%d\n",icnt);
+        icnt--;
+    }
+}
+int main()
+{
+    Display();
+
+    return 0 ;
+}
