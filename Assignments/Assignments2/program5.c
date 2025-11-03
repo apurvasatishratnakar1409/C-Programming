823ac32fd6904eeb9cd2e247396f8925c51606fd
Apurva Satish Ratnakar <apurvasatishratnakar1409@gmail.com>
2025-11-04 01:37:29 +0530

    program5.c
    User enters a number.
    
    Function CheckEven() checks if the number is divisible by 2.
    
    If divisible → true (1)
    
    If not divisible → false (0)
    
    Output shows 1 for even, 0 for odd.

---
+++ Assignments/Assignments2/program5.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckEven(int iNo)
+{
+    if(iNo % 2 == 0)
+    {
+        return true;  
+    }
+    else
+    {
+        return false;  
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter the number:\n");
+    scanf("%d",&iValue);
+
+    bRet = CheckEven(iValue);
+
+    printf("Result is %d\n", bRet);
+
+    return 0;
+}
